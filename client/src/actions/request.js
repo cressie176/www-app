08b8b9e668bae04be9e9c748bcae170c1bf02fa4
Stephen Cresswell <git@stephen-cresswell.net>
2017-09-18 23:35:13 +0100

    Set csrf token on origin requests

--- client/src/actions/request.js
+++ client/src/actions/request.js
@@ -1,10 +1,9 @@
 import Cookies from 'universal-cookie';
+import Url from 'url';
 
-export default async function request(url, { method = 'GET', credentials = 'same-origin', allowedStatusCodes = [], timeout = 5000, }) {
+export default async function request(url, { method = 'GET', headers = {}, credentials = 'same-origin', allowedStatusCodes = [], timeout = 5000, }) {
 
-  const cookies = new Cookies();
-  const csrfToken = cookies.get('x-csrf-token', { path: '/', });
-  const res = await fetch(url, { method, credentials, timeout, headers: { 'x-csrf-token': csrfToken, }, });
+  const res = await fetch(url, { method, credentials, timeout, headers: assignCsrfToken(headers), });
   switch (res.status) {
     case 200: {
       return await res.json();
@@ -16,6 +15,18 @@ export default async function request(url, { method = 'GET', credentials = 'same
     default: {
       if (allowedStatusCodes.indexOf(res.status) >= 0) return;
       throw new Error(`${url} returned ${res.status} ${res.statusText}`);
+    }
+  }
+
+  function assignCsrfToken(headers) {
+    const { host } = Url.parse(url);
+    if (host) return headers;
+
+    const cookies = new Cookies();
+    const csrfToken = cookies.get('x-csrf-token', { path: '/', });
+    return {
+      ...headers,
+      'x-csrf-token': csrfToken,
     }
   }
 }
