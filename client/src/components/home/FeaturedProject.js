bc3ac388327abc8b8c003a82fa76797f8749c3f7
Stephen Cresswell <git@stephen-cresswell.net>
2017-09-18 23:35:30 +0100

    Tidy up

--- client/src/components/home/FeaturedProject.js
+++ client/src/components/home/FeaturedProject.js
@@ -14,7 +14,7 @@ export class FeaturedProject extends React.Component {
     if (this.props.project.error) {
       return (
         <li className='list-group-item featured-project featured-project--error'>
-          <i className='fa fa-exclamation-triangle featured-project__downloads__icon' aria-hidden='true'></i>
+          <i className='fa fa-exclamation-triangle icon-text' aria-hidden='true'></i>
           <span className='featured-project__link'>Error loading module</span>
         </li>
       );
@@ -30,7 +30,7 @@ export class FeaturedProject extends React.Component {
         <li className='list-group-item featured-project featured-project--loading'>
           <span className='featured-project__link'>Loading…</span>
           <span className='featured-project__downloads featured-project__downloads--loading'>
-            <i className='fa fa-spinner fa-spin featured-project__downloads__icon' aria-hidden='true'></i>
+            <i className='fa fa-spinner fa-spin text-icon' aria-hidden='true'></i>
           </span>
           <div></div>
         </li>
@@ -46,20 +46,20 @@ export class FeaturedProject extends React.Component {
           {(() => {
             if (this.props.project.downloads_loading) {
               return <span className='featured-project__downloads featured-project__downloads--loading'>
-                <i className='fa fa-spinner fa-spin featured-project__downloads__icon' aria-hidden='true'></i>
+                <i className='fa fa-spinner fa-spin text-icon' aria-hidden='true'></i>
               </span>;
             } else if (this.props.project.downloads_missing) {
               return <span className='featured-project__downloads featured-project__downloads--missing'>
-                <i className='fa fa-chain-broken featured-project__downloads__icon' aria-hidden='true'></i>
+                <i className='fa fa-chain-broken text-icon' aria-hidden='true'></i>
               </span>;
             } else if (this.props.project.downloads_error) {
               return <span className='featured-project__downloads featured-project__downloads--error'>
-                <i className='fa fa-exclamation-triangle featured-project__downloads__icon' aria-hidden='true' title={this.props.project.downloads_error.message}></i>
+                <i className='fa fa-exclamation-triangle text-icon' aria-hidden='true' title={this.props.project.downloads_error.message}></i>
               </span>;
             } else if (this.props.project.downloads) {
               return <span className='featured-project__downloads featured-project__downloads--loaded'>
                 {this.props.project.downloads.toLocaleString()}
-                <i className='fa fa-download featured-project__downloads__icon' aria-hidden='true' title={`${this.props.project.downloads.toLocaleString()} downloads per month`}></i>
+                <i className='fa fa-download text-icon' aria-hidden='true' title={`${this.props.project.downloads.toLocaleString()} downloads per month`}></i>
               </span>;
             }
           })()}
