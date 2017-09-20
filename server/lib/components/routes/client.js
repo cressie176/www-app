6d98570445b56f229885c58eba3a6bd7d2e91716
Stephen Cresswell <git@stephen-cresswell.net>
2017-09-20 07:16:46 +0100

    Minor refactor

--- server/lib/components/routes/client.js
+++ server/lib/components/routes/client.js
@@ -7,18 +7,20 @@ module.exports = function() {
 
   function start({ app, config, logger, prepper,}, cb) {
 
-    const staticMiddleware = express.static('./client/build', { setHeaders: (res, path) => {
-      res.set('Cache-Control', 'public, max-age=3600, must-revalidate');
-    }, });
+    const staticMiddleware = express.static('./client/build', {
+      setHeaders: (res, path) => {
+        res.set('Cache-Control', 'public, max-age=3600, must-revalidate');
+      },
+    });
 
-    // We use StatusCake for monitoring site availability. No need to log
+    // Disable logging for StatusCake
     app.use((req, res, next) => {
       if (!req.headers['user-agent']) return next();
       if (!req.headers['user-agent'].includes('StatusCake')) return next();
       prepper.disable(req, res, next);
     });
 
-    // Make runtime client config available to index.html via script tag (yuck!)
+    // Make runtime client config available via script tag (yuck!)
     app.get('/config.js', app.locals.hasRole('guest'), (req, res) => {
       res.set('Content-Type' ,'application/javascript; charset=utf-8');
       res.set('Cache-Control', 'public, max-age=3600, must-revalidate');
@@ -28,13 +30,13 @@ module.exports = function() {
     // Always serve the app from root
     app.get('/index.html', (req, res) => res.redirect(301, '/'));
 
-    // Handle requests to the robots.txt, humans.txt and the client app
+    // Handle requests to the robots.txt, humans.txt and the client app without disabling logging
     app.get([/^\/$/, '/robots.txt', '/humans.txt',], app.locals.hasRole('guest'), staticMiddleware);
 
-    // Attempt to serve other static resources
+    // Serve other static resources with logging disabled
     app.get(CLIENT_REQUESTS, prepper.disable, app.locals.hasRole('guest'), staticMiddleware);
 
-    // Ensures client 404's are handled by the app
+    // Ensure client 404's are handled by the app
     app.get(CLIENT_REQUESTS, prepper.enable, app.locals.hasRole('guest'), (req, res, next) => {
       res.set('Cache-Control', 'public, max-age=3600, must-revalidate');
       res.status(404);
